68a48e730e0bcd841b787a1c5711cff49d86fd2c
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-06-11 07:21:56 +0300

    Added male names,female names and weekdays arrays

---
+++ dayfinder.js
@@ -0,0 +1,4 @@
+//Arrys to work with:
+weekDay=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var male=['Kwasi','Kwadwo', 'Kwabena', 'Kwaku','Yaw','Kofi','Kwame'];
+var female=['Akosua', 'Adwoa', 'Abenaa','Akua' ,'Yaa' ,'Afua','Ama'];
