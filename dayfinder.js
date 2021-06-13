0a0d62a0fd83f4cbb0ae1fb94e17597e286177b1
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-06-13 10:37:48 +0300

    Added more detailed description message

--- dayfinder.js
+++ dayfinder.js
@@ -4,8 +4,8 @@ var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
 function getData(e) {
 
-    var male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    // var male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    // var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
 
     let day = document.getElementById("day").value;
@@ -15,9 +15,9 @@ function getData(e) {
 
     var date = new Date(year, month, day);
     if (gender == "male") {
-        alert("Your Ghanian name is " + male[date.getDay()] + "and you were born on a " + weekDay[date.getDay()]);
+        alert("Your Ghanian "+gender+" name is " + male[date.getDay()] + "and you were born on a " + weekDay[date.getDay()]);
     }
     else if (gender == "female") {
-        alert("Your Ghanian name is " + female[date.getDay()] + " and you were born on a " + weekDay[date.getDay()]);
+        alert("Your Ghanian "+gender +"name is " + female[date.getDay()] + " and you were born on a " + weekDay[date.getDay()]);
     }
 }
