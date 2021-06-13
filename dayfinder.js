98d4a5b73b66c0cfa29d0220184ce67dead7e946
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-06-13 21:32:28 +0300

    update alert content

--- dayfinder.js
+++ dayfinder.js
@@ -4,9 +4,6 @@ var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
 function getData(e) {
 
-    // var male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    // var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-
 
     let day = document.getElementById("day").value;
     let month = document.getElementById("month").value;
@@ -14,10 +11,11 @@ function getData(e) {
     var gender = document.getElementById("gender").value;
 
     var date = new Date(year, month, day);
+   
     if (gender == "male") {
-        alert("Your Ghanian "+gender+" name is " + male[date.getDay()] + " and you were born on a " + weekDay[date.getDay()]);
+        alert("Your Akan " + gender + " name is " + male[date.getDay()] + " and you were born on a " + weekDay[date.getDay()]);
     }
     else if (gender == "female") {
-        alert("Your Ghanian "+gender +" name is " + female[date.getDay()] + " and you were born on a " + weekDay[date.getDay()]);
+        alert("Your Akan " + gender + " name is " + female[date.getDay()] + " and you were born on a " + weekDay[date.getDay()]);
     }
 }
